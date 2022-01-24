f6817c2f74e304ab403a1efa79f216fd9215926d
georgevictor90 <georgevictorl@yahoo.com>
2022-01-24 18:20:42 +0200

    added functions for storing player info

--- script.js
+++ script.js
@@ -3,10 +3,29 @@ const game = (() => {
   let currentPlayer;
   let playerName; 
   let playerMark;
+  let player1Name;
+  let player2Name;
   let player1Mark;
   let player2Mark;
 
-  let setGameMode = () => {
+  const player = (name, mark) => {
+    this.name = name;
+    this.mark = mark;
+    let currentPlayer
+    return {
+      name, 
+      mark,
+      currentPlayer: currentPlayer,
+    }
+  }
+
+  const player1 = player('name', 'mark');
+  const player2 = player('name2', 'mark2');
+  const newGameBtn = document.getElementById('newGameBtn');
+  const newGameForm = document.getElementById('newGameForm');
+  
+
+  const setGameMode = () => {
     if (document.getElementById('vsComputer').checked) {
       document.getElementById('vsComputerForm').classList.add('visible');
       document.getElementById('vsComputerForm').classList.remove('hidden');
@@ -22,21 +41,10 @@ const game = (() => {
       gameMode = 2
     }
     if (!gameMode) return
-    console.log('Game Mode: ' + gameMode)
     return gameMode
   }
 
-  let markSpace = (e) => {
-    
-    let index = gameBoard.gridTiles.indexOf(e.target);
-    gameBoard.gameBoardArr[index] = 'bang!';
-    gameBoard.render()
-    
-  }
-
-  document.getElementById('chooseMode').addEventListener('click', setGameMode)
-
-  document.getElementById('playerMark').addEventListener('click', () => {
+  const getMark1 = () => {
     if ( (!document.getElementById('X').checked) && (!document.getElementById('O').checked) ) return
 
     if (document.getElementById('X').checked) {
@@ -45,10 +53,9 @@ const game = (() => {
     if (document.getElementById('O').checked) {
       playerMark = "O"
     }
-    console.log('playerMark is ' + playerMark)
-  })
+  }
 
-  document.getElementById('player1Mark').addEventListener('click', () => {
+  const getMark2 = () => {
     if ( (!document.getElementById('p1-X').checked) && (!document.getElementById('p1-O').checked) ) return
 
     if (document.getElementById('p1-X').checked) {
@@ -59,21 +66,70 @@ const game = (() => {
       player1Mark = "O";
       player2Mark = "X"
     }
-    console.log('player1Mark is ' + player1Mark);
-    console.log('player2Mark is ' + player2Mark);
+  }
+  
+
+  const getPlayerNames = () => {
+    if (gameMode === 2) {
+      player1Name = document.getElementById('player1Name').value;
+      player2Name = document.getElementById('player2Name').value;
+    }
+    if (gameMode === 1) {
+      player1Name = document.getElementById('playerName').value;
+      player2Name = 'Computer';
+    }
+    
+  }
+
+  const createNewPlayers = () => {
+    player1.name = player1Name;
+    player1.mark = player1Mark;
+    player2.name = player2Name;
+    player2.mark = player2Mark;
+  }
+
+  const setCurrentPlayer = () => {
+    if (player1.mark === 'X') {
+      player1.currentPlayer = 'true';
+      player2.currentPlayer = 'false';
+
+    } else if (player2.mark = 'X') {
+      player2.currentPlayer = 'true';
+      player1.currentPlayer = 'false';
+    }
+  }
+
+  const markSpace = (e) => {
+    
+    let index = gameBoard.gridTiles.indexOf(e.target);
+    gameBoard.gameBoardArr[index] = 'bang!';
+    gameBoard.render()
+    
+  }
+
+  document.getElementById('chooseMode').addEventListener('click', setGameMode)
+
+  document.getElementById('playerMark').addEventListener('click', getMark1)
+
+  document.getElementById('player1Mark').addEventListener('click', getMark2)
+
+  newGameBtn.addEventListener('click', () => {
+    getPlayerNames();
+    createNewPlayers();
+    setCurrentPlayer();
   })
 
   return {
-    setGameMode,
     markSpace,
   }
+
 
 })()
 
 
 const gameBoard = (() => {
 
-  let gameBoardArr = ["", "", "", "O", "", "", "", "", ""];
+  let gameBoardArr = ["", "", "", "", "", "", "", "", ""];
   const gridTiles = Array.from(document.getElementsByClassName('gridTile'));
   const grid = document.getElementById('gameBoard');
 
@@ -84,6 +140,7 @@ const gameBoard = (() => {
     return gridTiles
   }
 
+  
   grid.addEventListener('click', game.markSpace);
 
 
@@ -95,14 +152,16 @@ const gameBoard = (() => {
   }
 })()
 
-const player = (name, mark) => {
-  this.name = name;
-  this.mark = mark;
-  return {
-    name, 
-    mark,
-  }
-}
+// const player = (name, mark) => {
+//   this.name = name;
+//   this.mark = mark;
+//   let currentPlayer
+//   return {
+//     name, 
+//     mark,
+//     currentPlayer: currentPlayer,
+//   }
+// }
 
 
 
